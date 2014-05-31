18eef14f0ed37448e3d15e2cf2c119fa63bd3379
leechannl <leechannl@gmail.com>
2014-05-31 13:39:31 +0800

    Add some globals.

--- Constans/main.m
+++ Constans/main.m
@@ -12,10 +12,14 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+
+        NSLog(@"\u03c0 is %f", M_PI);
+        NSLog(@"%d is larger", MAX(10, 20));
+
+        NSLocale *here = [NSLocale currentLocale];
+        NSString *currency = [here objectForKey:NSLocaleCurrencyCode];
+        NSLog(@"Money is %@", currency);
+
     }
     return 0;
 }
